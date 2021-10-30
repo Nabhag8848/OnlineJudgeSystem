6ef7a811b0ff8efcef51e21e0c1b14f0985c8290
unknown <kelvimanavadaria17012003@gmail.com>
2021-10-30 23:09:50 +0530

    LINKED ALL THE HTML AND STATIC FILES USING URLS AND VIEWS OF DJANGO

--- JudgeSystemProject/JudgeSystemApp/urls.py
+++ JudgeSystemProject/JudgeSystemApp/urls.py
@@ -3,10 +3,10 @@ from .import views
 
 urlpatterns=[
     path('',views.main,name="main"),
-    path('/index',views.index,name="index"),
-    path('/Problems',views.Problem,name="Problem"),
-    path('/Profile',views.Profile,name="Profile"),
-    path('/login',views.login_register,name="login"),
+    path('index/',views.index,name="index"),
+    path('Problem/',views.Problem,name="Problem"),
+    path('Profile/',views.Profile,name="Profile"),
+    path('login/',views.login_register,name="login"),
 
 
    
